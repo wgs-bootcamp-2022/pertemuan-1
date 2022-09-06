01b64c4cf37097a94ea1a1ef8baa20fa05400d7f
zakariyahya <yazakkars@gmail.com>
2022-09-06 20:30:03 +0700

    add comment

--- app.js
+++ app.js
@@ -1,25 +1,24 @@
 const fs = require('fs');
-fs.writeFileSync('test.txt','hello world secara synchronus');
 
-fs.readFile('test.txt', 'utf8', function(err, data){
+fs.writeFileSync('test.txt','hello world secara synchronus'); //membaca file pada file test.txt 
+
+fs.readFile('test.txt', 'utf8', function(err, data){ //membaca file pada file test.txt
     if (err) throw err;
     console.log(data);
 });
   
 const readline = require('readline');
 
-const rl = readline.createInterface({
+const rl = readline.createInterface({ 
     input: process.stdin,
     output: process.stdout
 })
 
-rl.question('What is your name? ', (name) =>{
-    rl.question('What is your tlp? ', (tlp) =>{
-        rl.question('Whats is your email? ', (email) => {
-            console.log(`Your name is ${name}, telephone is ${tlp} and your email is ${email}`);
-            rl.close();
+rl.question('What is your name? ', (name) =>{ //menginput dengan parameter name
+    rl.question('What is your tlp? ', (tlp) =>{ // //menginput dengan parameter telp
+        rl.question('Whats is your email? ', (email) => { //menginput dengan parameter email
+            console.log(`Your name is ${name}, telephone is ${tlp} and your email is ${email}`); //menampilkan hasil input pada cli
+            rl.close(); 
         })
     })
 })
-
-//besok npm, belajar membangun aplikasi menyimpan kontak CLI denga yax
